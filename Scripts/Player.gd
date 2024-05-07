9caadab1cb57ff37b6534314e417f02e61e7e4d5
Blurry <julian.lb.games@gmail.com>
2024-05-07 11:09:50 +0200

    Lesson11: Number Operations & More Printing

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,11 @@
 class_name Player extends RigidBody2D
 
 func _ready()-> void:
-	pass
+	test()
 
 func _process(delta: float)-> void:
 	pass
+
+func test()-> void:
+	print((2 + 3) * 5)
+	print("2 + 2 is: " + str(4))
