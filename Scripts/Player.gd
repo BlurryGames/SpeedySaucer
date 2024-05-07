6f22b4ca07ac25e3257d7855af28f5e38fae5d9f
Blurry <julian.lb.games@gmail.com>
2024-05-07 11:46:28 +0200

    Lesson15: Processing & apply_force()

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,4 +1,7 @@
 class_name Player extends RigidBody2D
 
 func _ready()-> void:
-	apply_impulse(Vector2(25.0, 0.0))
+	pass
+
+func _physics_process(_delta: float)-> void:
+	apply_force(Vector2(25.0, 0.0))
