09b79014970edf1c7ef3bf601569e2732670694f
Blurry <julian.lb.games@gmail.com>
2024-05-07 14:24:27 +0200

    Lesson18: Variable Scope

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,8 @@
 class_name Player extends RigidBody2D
 
+var force: float = 1500.0
+
 func _physics_process(_delta: float)-> void:
-	var force: float = 1000.0
 	if Input.is_action_pressed("MoveRight"):
 		apply_force(Vector2(force, 0.0))
 	if Input.is_action_pressed("MoveLeft"):
