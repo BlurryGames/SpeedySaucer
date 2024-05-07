3d1b8f201fbf1654421430bd383763c6335a1dca
Blurry <julian.lb.games@gmail.com>
2024-05-07 14:36:43 +0200

    Lesson19: Function Return Values

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,10 @@ class_name Player extends RigidBody2D
 
 var force: float = 1500.0
 
+func _ready()-> void:
+	var additionResult = add(10, 5)
+	print(additionResult)
+
 func _physics_process(_delta: float)-> void:
 	if Input.is_action_pressed("MoveRight"):
 		apply_force(Vector2(force, 0.0))
@@ -11,3 +15,10 @@ func _physics_process(_delta: float)-> void:
 		apply_force(Vector2(0.0, -force))
 	if Input.is_action_pressed("MoveDown"):
 		apply_force(Vector2(0.0, force))
+
+func test()-> int:
+	var t: int = 10
+	return t + 10
+
+func add(number1: int, number2: int)-> int:
+	return number1 + number2
