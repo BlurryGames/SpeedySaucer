8cb0e7004a154f4cb5354c8d16f2fff4fc09e6d1
Blurry <julian.lb.games@gmail.com>
2024-05-07 13:53:58 +0200

    Lesson16: If Statement & Comparison Operators

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,24 @@
 class_name Player extends RigidBody2D
 
 func _ready()-> void:
-	pass
+	var test: int = 5
+	if test > 5:
+		print("True")
+		print("Seconbd message")
+	elif test == 5:
+		print("Test is 5")
+	else:
+		print("False")
+	
+	var message: String = "Greet"
+	if message == "Hello":
+		print("Hi there")
+	elif message == "Bye":
+		print("Goodbye!")
+	elif message == "Thanks":
+		print("Thank you")
+	else:
+		print("Invalid message")
 
 func _physics_process(_delta: float)-> void:
 	apply_force(Vector2(25.0, 0.0))
