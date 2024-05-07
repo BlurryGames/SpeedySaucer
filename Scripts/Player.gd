3045028b2e6f7a7ada1ffd66cb6c6627e12b7303
Blurry <julian.lb.games@gmail.com>
2024-05-07 14:11:24 +0200

    Lesson17: Getting Input

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,12 @@
 class_name Player extends RigidBody2D
 
-func _ready()-> void:
-	var test: int = 5
-	if test > 5:
-		print("True")
-		print("Seconbd message")
-	elif test == 5:
-		print("Test is 5")
-	else:
-		print("False")
-	
-	var message: String = "Greet"
-	if message == "Hello":
-		print("Hi there")
-	elif message == "Bye":
-		print("Goodbye!")
-	elif message == "Thanks":
-		print("Thank you")
-	else:
-		print("Invalid message")
-
 func _physics_process(_delta: float)-> void:
-	apply_force(Vector2(25.0, 0.0))
+	var force: float = 1000.0
+	if Input.is_action_pressed("MoveRight"):
+		apply_force(Vector2(force, 0.0))
+	if Input.is_action_pressed("MoveLeft"):
+		apply_force(Vector2(-force, 0.0))
+	if Input.is_action_pressed("MoveUp"):
+		apply_force(Vector2(0.0, -force))
+	if Input.is_action_pressed("MoveDown"):
+		apply_force(Vector2(0.0, force))
